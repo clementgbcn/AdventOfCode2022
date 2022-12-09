ea55ccb4bc9cd55d72989bdac76be760d2b9b313
Clement Gaboriau Couanau <clement.gaboriaucouanau@datadoghq.com>
2022-12-09 15:27:36 +0100

    Add Day 9 visualization

--- Day09.py
+++ Day09.py
@@ -1,4 +1,7 @@
+import curses
+
 from Day import Day
+
 
 # Day		Star	Test Type	          Result	|	Elapsed Time
 #   9		1st		Example		              13	|	 0.127ms
@@ -8,7 +11,6 @@ from Day import Day
 
 
 class Knot:
-
     MOVE_MAP = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
 
     def __init__(self):
@@ -24,33 +26,62 @@ class Knot:
         return self.x, self.y
 
     def get_close(self, knot):
-        if abs(knot.x-self.x) > 1:
+        if abs(knot.x - self.x) > 1:
             self.x += 1 if knot.x > self.x else -1
-            if knot.y != self.y:
-                self.y += 1 if knot.y > self.y else -1
-        elif abs(knot.y-self.y) > 1:
+            self.y += 1 if knot.y > self.y else (-1 if knot.y != self.y else 0)
+        elif abs(knot.y - self.y) > 1:
             self.y += 1 if knot.y > self.y else -1
-            if knot.x != self.x:
-                self.x += 1 if knot.x > self.x else -1
+            self.x += 1 if knot.x > self.x else (-1 if knot.x != self.x else 0)
 
 
 class Rope:
+    SIZE_ARRAY = 20
 
-    def __init__(self, nb_knot):
+    def __init__(self, nb_knot, should_display=False):
         self.rope = [Knot() for _ in range(nb_knot)]
         self.length = nb_knot
         self.visited_place = {self.rope[-1].get_pos()}
+        self.should_display = should_display
+        if self.should_display:
+            self.screen = curses.initscr()
+            curses.noecho()
+            curses.cbreak()
+            curses.start_color()
+            curses.use_default_colors()
+            for i in range(0, curses.COLORS):
+                curses.init_pair(i + 1, i, -1)
+            self.screen.keypad(True)
+            self.screen.idcok(False)
 
-    def move_head(self,direction):
+    def move_head(self, direction):
         self.rope[0].move(direction)
         for i in range(1, self.length):
-            self.rope[i].get_close(self.rope[i-1])
+            self.rope[i].get_close(self.rope[i - 1])
+        self.display()
         self.visited_place.add(self.rope[-1].get_pos())
 
     def move_rope(self, input_value):
-        for order in input_value:
-            for _ in range(int(order[2:])):
-                self.move_head(order[0])
+        list(map(lambda x: [self.move_head(x[0]) for _ in range(int(x[2:]))], input_value))
+
+    def draw_char(self, x, y, char, color=0):
+        xp = x + curses.COLS // 2 - 60
+        yp = curses.LINES // 2 - y + 130
+        if xp < 0 or xp > curses.COLS - 1 or yp < 0 or yp > curses.LINES - 1:
+            return
+        self.screen.move(yp, xp)
+        self.screen.addstr(char, curses.color_pair(color))
+
+    def display(self):
+        if not self.should_display:
+            return
+        self.screen.erase()
+        for (x, y) in self.visited_place:
+            self.draw_char(x, y, "#")
+        for knot in self.rope[1:-1]:
+            self.draw_char(knot.x, knot.y, "X", 84)
+        self.draw_char(self.rope[-1].x, self.rope[-1].y, "T", 84)
+        self.draw_char(self.rope[0].x, self.rope[0].y, "H", 198)
+        self.screen.refresh()
 
 
 class Day09(Day):
