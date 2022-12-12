ddddcfe025bab75d6b79b8ad3b5c5dec40cc162d
Clement Gaboriau Couanau <clement.gaboriaucouanau@datadoghq.com>
2022-12-12 09:01:10 +0100

    Reduce code size

--- Day12.py
+++ Day12.py
@@ -7,8 +7,6 @@ from Day import Day
 class Cartography:
     def __init__(self, input_value):
         self.height = []
-        self.start = (None, None)
-        self.end = (None, None)
         self.shortest_path = []
         for row_idx, row in enumerate(input_value):
             row_copy = list(row)
@@ -24,36 +22,31 @@ class Cartography:
             self.shortest_path.append([sys.maxsize for _ in range(len(row_copy))])
         self.shortest_path[self.end[0]][self.end[1]] = 0
         self.visited = set()
-        self.to_visit = [self.end]
+        self.to_visit = [(self.end[0], self.end[1], 0)]
         self.minimal_starting = []
         for i in range(len(self.height)):
-            for j in range(len(self.height[i])):
-                if self.height[i][j] == "a":
-                    self.minimal_starting.append((i, j))
+            self.minimal_starting += [
+                (i, j) for j in range(len(self.height[i])) if self.height[i][j] == "a"
+            ]
 
-    def find_way(self, x, y):
+    def find_way_and_update(self, x, y, path):
+        self.shortest_path[x][y] = min(path + 1, self.shortest_path[x][y])
         if (x, y) in self.visited:
             return
         self.visited.add((x, y))
         path = self.shortest_path[x][y]
         height_max = ord(self.height[x][y]) - 1
-        if x > 0 and height_max <= ord(self.height[x - 1][y]):
-            self.shortest_path[x - 1][y] = min(path + 1, self.shortest_path[x - 1][y])
-            self.to_visit.append((x - 1, y))
-        if x < len(self.height) - 1 and height_max <= ord(self.height[x + 1][y]):
-            self.shortest_path[x + 1][y] = min(path + 1, self.shortest_path[x + 1][y])
-            self.to_visit.append((x + 1, y))
-        if y > 0 and height_max <= ord(self.height[x][y - 1]):
-            self.shortest_path[x][y - 1] = min(path + 1, self.shortest_path[x][y - 1])
-            self.to_visit.append((x, y - 1))
-        if y < len(self.height[0]) - 1 and height_max <= ord(self.height[x][y + 1]):
-            self.shortest_path[x][y + 1] = min(path + 1, self.shortest_path[x][y + 1])
-            self.to_visit.append((x, y + 1))
+        for i, j in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            if (i != 0 and 0 <= x + i < len(self.height)) or (
+                j != 0 and 0 <= (y + j) < len(self.height[0])
+            ):
+                if height_max <= ord(self.height[x + i][y + j]):
+                    self.to_visit.append((x + i, y + j, path))
 
     def visit_graph(self):
         while len(self.to_visit) > 0:
-            x, y = self.to_visit.pop(0)
-            self.find_way(x, y)
+            x, y, path = self.to_visit.pop(0)
+            self.find_way_and_update(x, y, path)
 
     def find_way_from_end(self):
         self.visit_graph()
